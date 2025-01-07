e318ecfae36f6ba28e3d5e61c308b80c06945fb4
myz1237 <mengyizhuoprivate@outlook.com>
2025-01-07 22:22:36 +0900

    feat: add prettier to format code

--- test/token.validation.unit.spec.ts
+++ test/token.validation.unit.spec.ts
@@ -3,46 +3,42 @@ import TokenSchema from '../tokenExpectedSchema.json'
 import DenyTokenSchema from '../denyTokenExpectedSchema.json'
 import Ajv from 'ajv'
 
-const reduceTokens = (path: string) => 
-    fs.readdirSync(path)
-        .map(list => JSON.parse(fs.readFileSync(`${path}/${list}`, 'utf8')))
-        .reduce((allTokens, currentList) => [...allTokens, ...Object.values(currentList)], [])
+const reduceTokens = (path: string) =>
+  fs
+    .readdirSync(path)
+    .map((list) => JSON.parse(fs.readFileSync(`${path}/${list}`, 'utf8')))
+    .reduce((allTokens, currentList) => [...allTokens, ...Object.values(currentList)], [])
 
-describe('Token validation', () => { 
-    describe('should validate all tokens', () => {
-        const allTokens = reduceTokens('./tokens')
-        const tokenValidator = new Ajv().compile(TokenSchema)
-        test.each(
-            allTokens.map((token: any) => [
-                // Used for test naming
-                token.name ?? token.address ?? 'Unknown token',
-                token.chainId ?? 'Unknown chain',
-                // --------------------
-                token
-            ])
-        )(
-            'Should be a valid token %s on chain %s', (_, __, token) => {
-                expect(tokenValidator(token)).toBeTruthy()
-            }
-        )
+describe('Token validation', () => {
+  describe('should validate all tokens', () => {
+    const allTokens = reduceTokens('./tokens')
+    const tokenValidator = new Ajv().compile(TokenSchema)
+    test.each(
+      allTokens.map((token: any) => [
+        // Used for test naming
+        token.name ?? token.address ?? 'Unknown token',
+        token.chainId ?? 'Unknown chain',
+        // --------------------
+        token,
+      ])
+    )('Should be a valid token %s on chain %s', (_, __, token) => {
+      expect(tokenValidator(token)).toBeTruthy()
     })
+  })
 
-    describe('should validate all deny tokens', () => {
-        const allDenyTokens = reduceTokens('./denyTokens')
-        const denyTokenValidator = new Ajv().compile(DenyTokenSchema)
-        test.each(
-            allDenyTokens.map((token: any) => [
-                // Used for test naming
-                token.name ?? token.address ?? 'Unknown token',
-                token.chainId ?? 'Unknown chain',
-                // --------------------
-                token
-            ])
-        )(
-            'Should be a valid deny token %s on chain %s', (_, __, token) => {
-                expect(denyTokenValidator(token)).toBeTruthy()
-            }
-        )
+  describe('should validate all deny tokens', () => {
+    const allDenyTokens = reduceTokens('./denyTokens')
+    const denyTokenValidator = new Ajv().compile(DenyTokenSchema)
+    test.each(
+      allDenyTokens.map((token: any) => [
+        // Used for test naming
+        token.name ?? token.address ?? 'Unknown token',
+        token.chainId ?? 'Unknown chain',
+        // --------------------
+        token,
+      ])
+    )('Should be a valid deny token %s on chain %s', (_, __, token) => {
+      expect(denyTokenValidator(token)).toBeTruthy()
     })
-
+  })
 })
