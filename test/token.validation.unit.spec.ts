ca38e9343ce913c002ffc6e90a92554ac289799e
Alex1237 <54893307+myz1237@users.noreply.github.com>
2025-06-25 22:04:06 +0800

    feat: add test to ensure chainId is the same in each file

--- test/token.validation.unit.spec.ts
+++ test/token.validation.unit.spec.ts
@@ -4,17 +4,24 @@ import DenyTokenSchema from '../denyTokenExpectedSchema.json'
 import Ajv from 'ajv'
 import { ChainId } from '@lifi/types'
 
+const tokensInEachFile = (path: string) =>
+  fs.readdirSync(path).map((fileName) => ({
+    fileName,
+    contents: JSON.parse(fs.readFileSync(`${path}/${fileName}`, 'utf8')),
+  }))
+
 const reduceTokens = (path: string) =>
-  fs
-    .readdirSync(path)
-    .map((list) => JSON.parse(fs.readFileSync(`${path}/${list}`, 'utf8')))
-    .reduce((allTokens, currentList) => [...allTokens, ...Object.values(currentList)], [])
+  tokensInEachFile(path).reduce(
+    (allTokens, { contents }) => [...allTokens, ...Object.values(contents)],
+    []
+  )
 
 describe('Token validation', () => {
   describe('should validate all tokens', () => {
     const allTokens = reduceTokens('./tokens')
     const tokenValidator = new Ajv().compile(TokenSchema)
-    test.each(
+
+    it.each(
       allTokens.map((token: any) => [
         // Used for test naming
         token.name ?? token.address ?? 'Unknown token',
@@ -22,15 +29,23 @@ describe('Token validation', () => {
         // --------------------
         token,
       ])
-    )('Should be a valid token %s on chain %s', (_, __, token) => {
+    )('should be a valid token %s on chain %s', (_, __, token) => {
       expect(tokenValidator(token)).toBeTruthy()
     })
+
+    it.each(tokensInEachFile('./tokens'))(
+      'should have the same chainId in $fileName file',
+      ({ contents }) => {
+        const chainIds = contents.map((token: any) => token.chainId)
+        expect(new Set(chainIds).size).toBe(1)
+      }
+    )
   })
 
   describe('should validate all deny tokens', () => {
     const allDenyTokens = reduceTokens('./denyTokens')
     const denyTokenValidator = new Ajv().compile(DenyTokenSchema)
-    test.each(
+    it.each(
       allDenyTokens.map((token: any) => [
         // Used for test naming
         token.name ?? token.address ?? 'Unknown token',
@@ -44,7 +59,7 @@ describe('Token validation', () => {
   })
 })
 
-describe.only('Token File Name Validation', () => {
+describe('Token File Name Validation', () => {
   const tokenFileNames = fs.readdirSync('./tokens')
   // Remove .json from file name
   const chainNamesFromTokenFileNames = tokenFileNames.map((fileName) =>
@@ -52,6 +67,6 @@ describe.only('Token File Name Validation', () => {
   )
   const chainNamesFromTypes = Object.keys(ChainId)
   it.each(chainNamesFromTokenFileNames)('file name %s.json should be valid', (chainName) => {
-    expect(chainNamesFromTypes.includes(chainName)).toBe(true)
+    expect(chainNamesFromTypes.includes(chainName)).toBeTruthy()
   })
 })
